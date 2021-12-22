67a2579874e22f786192fd3732d4a39ce2f89334
MxZhyr <82393939+MxZhyr@users.noreply.github.com>
2021-12-22 13:02:22 +0200

    Update test_all.py

--- main/test_all.py
+++ main/test_all.py
@@ -7,6 +7,7 @@ import main
 @pytest.mark.parametrize("n, m", [("2", "Ф"),
                                   ("A", "3")])
 def test_size_isnumeric(n, m):
+    import main
     with pytest.raises(main.SizeError):
         result = main.size_mtr(n, m)
 
@@ -17,23 +18,27 @@ def test_size_isnumeric(n, m):
     ['1', '2', '3', "A"]
 ])
 def test_matrix_isnumeric(args):
+    import main
     # Создать инстанс класса и прокинуть в него зачения? --- можно но реализовано через дополнительній чек
     with pytest.raises(main.EntrError):
         result = main.check_for_error(args)
 
 
 def test_choise():
+    import main
     with pytest.raises(main.ChoiseError):
         result = main.choise_operation(main.Matrix(1, 1, ["1"]), "-1")
 
 
 def test_multiply():
+    import main
     testmtr = main.Matrix(1, 1, [1])
     with pytest.raises(main.MultiplyError):
         result = testmtr.multiply("A")
 
 
 def test_positive_opr():
+    import main
     testmtr = main.Matrix(2, 2, [1, 0, 0, 1])
     result = testmtr.determinant(testmtr.matrix)
     assert result == 1
